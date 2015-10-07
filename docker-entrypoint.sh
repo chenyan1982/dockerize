820c6306214094c41c298e9b8c53c38c998978d8
陈彦 <564196013@qq.com>
2015-10-07 14:50:08 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,16 +2,12 @@
 
 mkdir $HUBOT_NAME
 cd $HUBOT_NAME
-yo hubot --owner="Sky.Y.Chen <chenyanhasmail@gmail.com>" \
-	--name="iSoft" \
-	--description="丫丫机器人" \
-	--adapter=campfire \
-	--default
+#yo hubot --owner="Sky.Y.Chen <chenyanhasmail@gmail.com>" \
+#	--name="iSoft" \
+#	--description="丫丫机器人" \
+#	--adapter=campfire \
+#	--default
 	
-sed -i '/hubot-heroku-keepalive/d' external-scripts.json
+#sed -i '/hubot-heroku-keepalive/d' ./external-scripts.json
 
-echo $(pwd)
-echo $(ls)
-cd bin
-echo $(ls)
-hubot
+#./bin/hubot

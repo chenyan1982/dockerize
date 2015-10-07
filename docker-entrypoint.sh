fef5e535dce05aad22062c0586e1b3dee3f1a262
陈彦 <564196013@qq.com>
2015-10-07 19:50:25 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,7 +6,7 @@ then
 else
 	mkdir $HUBOT_NAME
 	cd $HUBOT_NAME
-	yes | yo hubot --owner="Sky.Y.Chen <chenyanhasmail@gmail.com>" \
+	yo hubot --owner="Sky.Y.Chen <chenyanhasmail@gmail.com>" \
 		--name="iSoft" \
 		--description="坚力软件" \
 		--adapter=campfire \
@@ -15,5 +15,5 @@ else
 	sed -i '/hubot-heroku-keepalive/d' ./external-scripts.json
 fi
 
-./bin/hubot
+bin/hubot
 

0e181aba8311690decb1d151b7350d529530306a
陈彦 <564196013@qq.com>
2015-10-07 20:48:49 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,7 @@ else
 	mkdir $HUBOT_NAME
 	cd $HUBOT_NAME
 	yes | yo hubot --owner="Sky.Y.Chen <chenyanhasmail@gmail.com>" \
-		--name="iSoft" \
+		--name="isoft" \
 		--description="坚力软件" \
 		--adapter=campfire \
 		--defaults
